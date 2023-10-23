ae6116f8d501f7e87207d76dd626aa2e6b4f499a
rayzathegrave <rdgraaf213@gmail.com>
2023-10-23 12:01:26 +0200

    huiswerk afgemaakt

--- loops.js
+++ loops.js
@@ -11,9 +11,13 @@
 // Tip: je kunt de .repeat() methode gebruiken om een karakter een n aantal keer te herhalen...
 // Dit heb je nog niet geleerd, maar bekijk hiervoor dit MDN-artikel eens: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
 // ==========================================
-
-
-
+for (let i = 1; i <= 5; i++) {
+    let row = '';
+    for (let j = 1; j <= i; j++) {
+        row += '*';
+    }
+    console.log(row);
+}
 
 
 // ==========================================
@@ -26,7 +30,13 @@
 // klaar!
 // ==========================================
 
-
+for (let i = 1; i <= 5; i++) {
+    if (i < 5) {
+        console.log("loop...");
+    } else {
+        console.log("klaar!");
+    }
+}
 
 
 
@@ -45,7 +55,15 @@
 // >> 8
 // >> 9
 // ==========================================
-
+for (let i = 0; i < 10; i++) {
+    if (i < 3) {
+        console.log(i);
+    } else if (i < 6) {
+        console.log("> " + i);
+    } else {
+        console.log(">> " + i);
+    }
+}
 
 
 
